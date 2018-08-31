594ff49c5de95962287388bd1223ba18d7d112e6
Muhammad Orabi <muhammad.orabi.2014@hotmail.com>
2018-09-01 00:52:58 +0200

    update testing config

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -4,9 +4,11 @@ namespace Tests\Feature;
 
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 class ExampleTest extends TestCase
 {
+    use DatabaseMigrations;
     /**
      * A basic test example.
      *
@@ -15,7 +17,6 @@ class ExampleTest extends TestCase
     public function testBasicTest()
     {
         $response = $this->get('/');
-
-        $response->assertStatus(200);
+        $response->assertStatus(404);
     }
 }
